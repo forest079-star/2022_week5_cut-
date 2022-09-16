dffcaca3403c12543622322374b8fc6ecc759294
forest079 <bluegx08@gmail.com>
2022-09-16 18:27:11 +0800

    初始設定-0916

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
     "./app/**/*.{html,ejs}"
   ],
   theme: {
-    // screens: {
-    //   sm: '540px',
-    //   md: '720px',
-    //   lg: '960px',
-    //   xl: '1140px',
-    //   '2xl': '1320px'
-    // },
+    container: {
+      center: true,    // 水平置中
+      padding: '12px', // 水平間距
+    },
+    screens: {
+      sm: '768px',
+      md: '992px',
+      lg: '1440px',
+      // sm: '540px',
+      // md: '720px',
+      // lg: '960px',
+      // xl: '1140px',
+      // '2xl': '1320px'
+    },
     // fontSize: {
     //   sm: ['14px', '21px'],
     //   base: ['16px', '24px'],
@@ -31,16 +38,38 @@ module.exports = {
       colors: {
         black: colors.black,
         white: colors.white,
-        gray: colors.gray,
+        // gray: colors.gray,
         // 'primary': {
         //   'dark': '#',
         //   DEFAULT: '#',
         //   'light': '#'
         // }
-        primary:   '#62DB54', // 綠色
-        secondary: '#09ACF5', // 藍色
-        // gray:      '#1C1C1C',
-        // dark:      '#2D2D2D',
+        primary:'#62DB54',
+        secondary:'#09ACF5',
+        cyan: '#494755',
+        'dark': {
+          DEFAULT: '#000000',
+          '100': '#2D2D2D',
+          '200': '#2A1E17',
+          '300': '#202020',
+          '400': '#1C1C1C',
+          '500': '#1A1A1A'
+        },
+        'gray': {
+          DEFAULT: '#C1C1C1',
+          '100': '#49475514',
+          '200': '#00000019',
+          '300': '#00000012',
+          '400': '#CDD0C4',
+          '500': '#00000034',
+          '600': '#C1C1C1',
+          '700': '#00000058',
+          '800': '#909090',
+          '900': '#7C7E77'
+        },
+      },
+      listStyleType: {
+        square: 'square' // list 樣式
       },
       fontSize: {
         // '3xxl': ['32px', '48px'],
@@ -48,16 +77,13 @@ module.exports = {
         // '6xxl': ['64px', '96px']
       },
       fontFamily: {
-        // 'azeret': ['"Azeret Mono"'], //新增 字型
+        'azeret': '"Azeret Mono" , monospace',
+        'noto': '"Noto Sans TC" , sans-serif',
       },
       dropShadow: {
         // 'card-shadow': '0px 2px 6px rgba(0, 0, 0, 0.29)'
       }
     },
-    container: {
-      center: true, // 水平置中
-      padding: '12px', // 水平間距
-    }
   },
   plugins: [
     require('@tailwindcss/forms'),
